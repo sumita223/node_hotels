bf940f8c32dd9ad6152cd60317f26ab5f3ca4e29
KIIT <kiit>
2024-03-09 22:45:43 +0530

    shift mongodb to online server

--- db.js
+++ db.js
@@ -1,14 +1,15 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 //define the mongodb url connection
-const mongoURL=  'mongodb://localhost:27017/hotels'
-
-
+//const mongoURL= process.env.MONGODB_URL_LOCAL //Replace 'mydatabase' with your database name
+const mongoURL= process.env.MONGODB_URL;
 
 mongoose.connect(mongoURL,{
     useNewUrlParser:true,
     useUnifiedTopology:true
 })
+
 
 const db=mongoose.connection;
 
--- server.js
+++ server.js
@@ -3,16 +3,17 @@ const express = require('express')
 const app=express();
 const db = require('./db');
 
+require('dotenv').config();
+
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
 
+const PORT = process.env.PORT ||3000;
 
 app.get("/",(req, res)=>{
     res.send("<h1>Welcome to our hotel</h1>");
 })
-
-
 
 //import the router files
 const personRoutes= require('./routes/personRoutes');
@@ -22,6 +23,6 @@ const menuItemRoutes= require('./routes/menuItemRoutes');
 app.use('/person', personRoutes);
 app.use('/menu', menuItemRoutes);
 
-app.listen(3000,()=>{
+app.listen(PORT,()=>{
     console.log(`The server is running`);
 })
